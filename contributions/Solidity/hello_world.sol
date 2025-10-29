b190f332365a195af0b41291f4344ccbcd91d93a
Shiva Sharma <shivaumj@gmail.com>
2025-10-30 02:54:15 +0530

    Your commit message

---
+++ contributions/Solidity/hello_world.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract HelloWorld {
+    function greet() external pure returns (string memory) {
+        return "Hello, World!";
+    }
+}
+
+
